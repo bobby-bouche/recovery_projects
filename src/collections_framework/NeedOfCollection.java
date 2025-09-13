228275b3d2665ecffb1933881414a00301b4fea1
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2025-09-12 21:16:08 -0700

    add collections framework for learning collections

--- src/collections_framework/NeedOfCollection.java
+++ src/collections_framework/NeedOfCollection.java
@@ -3,8 +3,27 @@ package collections_framework;
 public class NeedOfCollection {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+
+		int[] myArray = new int[10000];
+		
+		Student[] students = new Student[10];
+		
+		students[0] = new Student();
+		students[1] = new Student();
+		
+		Object[] objects = new Object[10];
+		
+		
 
 	}
 
+}
+
+class Book{
+	
+}
+
+class Student{
+	
 }
